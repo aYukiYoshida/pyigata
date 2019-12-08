1f129592d927a96dbf6a77762d476b538406a9a9
aYukiYoshida <yuki.yoshida@access-company.com>
2019-12-08 12:16:33 +0900

    [add] Add rename method to setup shell script

--- setup.sh
+++ setup.sh
@@ -23,13 +23,20 @@ logger(){
 }
 
 usage(){
-    echo "USAGE: ${COMMANDNAME} [OPTION] <PLATFORM>"
+    echo "USAGE: ${COMMANDNAME} [OPTION] <COMMAND>"
     echo ""
     echo "OPTIONS"
     echo "  -h,--help       Show this help script"
     echo "  -u,--usage      Synonymous with -h or --help"
     echo "  -d,--debug      Show debug logs"    
     echo ""
+    echo "COMMANDS"
+    echo "  build            build the target"
+    echo "  install          install the target"
+    echo "  test             run the unit-test"
+    echo "  clean            clean the target"
+    echo "  check            check the target"
+    echo "  rename <NAME>    rename the target"
     exit 0
 }
 
@@ -37,12 +44,34 @@ abort(){
     local evt=$1
     case ${evt} in
         "ExceededCommand") local message="Input commands were exceeded !!" ;;
+        "LackingCommand") local message="Input command was lacking !!" ;;
         "InvalidCommand")  local message="Invalid input command !!" ;;
         "InvalidOption")   local message="Invalid input option !!" ;;
         *)                 local message="Abort !!" ;;
     esac
     logger 2 "${message}"
     exit 1
+}
+
+rename(){
+    local FLG_B=$1
+    local name=$2
+
+    if [ -z ${name} ];then
+        abort LackingCommand
+    else
+        if [ ${FLG_B} -ne 1 ];then
+            logger 1 "Rename target to ${name}"
+            mv sample ${name}
+            sed -i "/name=/s/sample/${name}/" .setup.py
+            for test_file in tests/*.py;do
+                sed -i "s/import sample/import ${name}/g" ${test_file}
+                sed -i "s/sample\./${name}\./g" ${test_file}
+            done
+        else
+            logger 0 "new name = ${name}"
+        fi
+    fi
 }
 
 ################################################################################
@@ -70,29 +99,36 @@ done
 logger 0 "\$@=$@"
 logger 0 "\$#=$#"
 
-if [ $# -gt 1 ];then
-    abort ExceededCommand
-else
-    input=$@
-    logger 0 "input=${input}"
-    cd ${DIRECTORY}
-    if [[ ${input} = build ]]||[[ ${input} = install ]]||[[ ${input} = test ]]||[[ ${input} = clean ]]||[[ ${input} = check ]]||[[ ${input} = sdist ]];then
-        if [ ${FLG_B} -ne 1 ];then
-            which pandoc 2>&1 > /dev/null
-            if [ $? -eq 0 ];then
-                [ -e README.md ] && pandoc --from markdown --to rst README.md -o README.rst
-            else
-                logger 2 "The \"pandoc\" comand is needed."
-                abort
-            fi
-            python3 ./.setup.py ${input}
-            rm -rf README.rst
-        else
-            logger 0 "valid input for setup = ${input}"
-        fi
+input=$1
+logger 0 "input=${input}"
+cd ${DIRECTORY}
+if [[ ${input} = build ]]||[[ ${input} = install ]]||[[ ${input} = test ]]||[[ ${input} = clean ]]||[[ ${input} = check ]]||[[ ${input} = sdist ]]||[[ ${input} = rename ]];then
+    if [[ ${input} = rename ]];then
+        name=$2
+        logger 0 "name=${name}"
+        logger 0 "length of name = ${#name}"
+        rename ${FLG_B} ${name}
     else
-        abort InvalidCommand
+        if [ $# -gt 1 ];then
+            abort ExceededCommand
+        else
+            if [ ${FLG_B} -ne 1 ];then
+                which pandoc 2>&1 > /dev/null
+                if [ $? -eq 0 ];then
+                    [ -e README.md ] && pandoc --from markdown --to rst README.md -o README.rst
+                else
+                    logger 2 "The \"pandoc\" comand is needed."
+                    abort
+                fi
+                python3 ./.setup.py ${input}
+                rm -rf README.rst
+            else
+                logger 0 "valid input for setup = ${input}"
+            fi
+        fi
     fi
+else
+    abort InvalidCommand
 fi
 
 # EOF
