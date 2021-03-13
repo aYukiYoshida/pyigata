670db3e09e5e4a3fb430b57f213ac95801ab1996
aYukiYoshida <yuki.yoshida@access-company.com>
2021-03-13 17:57:06 +0900

    [add] Implement class of ConstantMeta

--- src/constant.py
+++ src/constant.py
@@ -12,3 +12,10 @@ class Constant(object):
         if name in self.__dict__:
             raise self.ConstError(f'Cannot rebind constant ({name})')
         self.__dict__[name] = value
+
+class ConstantMeta(type):
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            raise TypeError(f'Cannot rebind constant ({name})')
+        else:
+            self.__setattr__(name, value)
