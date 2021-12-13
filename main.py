4a3e4b4d750f5cf97501b356f09c18280a9198e9
aYukiYoshida <yuki.yoshida@access-company.com>
2021-12-13 11:02:43 +0900

    [add] Introduce subparsers to argument parsing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 import os
 import pathlib
@@ -10,50 +10,89 @@ import src
 
 SCRIPT_FILE_PATH = pathlib.Path(os.path.abspath(__file__))
 
-def main(argv):
-    if argv.debug:
-        argv.log_level = 0
-    logger = src.common.Common(argv.log_level)
 
-    if argv.command == 'license':
-        if argv.target:
-            logger.info(f'TARGET: {argv.target}')
-            logger.info(f'OUTPUT: {argv.output}')
-            creator = src.LicenseListCreator(
-                python_path=argv.target,
-                output_csv=argv.output,
-                log_level=argv.log_level)
-            creator()
-        else:
-            logger.error('Please input --target')
-            logger.abort('Try --help to get a list of flags !!')
-    elif argv.command == 'test':
-        message = f'testing in mode of {argv.test_mode}'
-        logger.info(f'{argv.name} is {message}')
-        logger.debug(f'{argv.name} is {message} with debug mode')
+def get_logger(args):
+    log_level = 0 if args.debug else args.log_level
+    return src.common.Common(log_level)
+
+
+def test_func(args):
+    logger = get_logger(args)
+    message = f"testing in mode of {args.mode}"
+    logger.info(f"{args.name} is {message}")
+    logger.debug(f"{args.name} is {message} with debug mode")
+
+
+def create_license_list(args):
+    logger = get_logger(args)
+    if args.target:
+        logger.info(f"TARGET: {args.target}")
+        logger.info(f"OUTPUT: {args.output}")
+        creator = src.LicenseListCreator(
+            python_path=args.target, output_csv=args.output, log_level=args.log_level
+        )
+        creator()
     else:
-        logger.error('Invalid command !!')
-        logger.abort('Try --help to get a list of flags !!')
+        logger.error("Please input --target")
+        logger.abort("Try --help to get a list of flags !!")
 
 
-def define_flags():
-    parser = ArgumentParser()
-    parser.add_argument('command', type=str,
-        choices=['test', 'license'], metavar='CMD', default=None, help='Command')
-    parser.add_argument('-d', '--debug', action='store_true', default=False,
-        help='Run debug mode.')
-    parser.add_argument('-l', '--log-level', type=int, default=1, metavar='NUM',
-        choices=range(4), help='Specify log level as an integer number.')
-    parser.add_argument('-n', '--name', type=str, default='Yuki Yoshida',
-        help='Your name.')
-    parser.add_argument('-o', '--output', type=str, default='license_list.csv',
-        help='Output licence list file name.')
-    parser.add_argument('-t', '--target', type=str, default=None,
-        metavar='PATH', help='Path to target python packages.')
-    parser.add_argument('--test-mode', type=str, default=0, choices=range(9),
-        metavar='NUM', help='Specify test mode as an integer number.')
-    return parser.parse_args()
+def main():
+    common_parser = ArgumentParser(description="parse common options.", add_help=False)
+    common_parser.add_argument(
+        "-d", "--debug", action="store_true", default=False, help="Run debug mode."
+    )
+    common_parser.add_argument(
+        "-l",
+        "--log-level",
+        type=int,
+        default=1,
+        metavar="NUM",
+        choices=range(4),
+        help="Specify log level as an integer number.",
+    )
 
-if __name__ == '__main__':
-    args = define_flags()
-    sys.exit(main(args))
+    main_parser = ArgumentParser(description="Sample command.")
+    subparsers = main_parser.add_subparsers(help="sub-command help")
+
+    # for test
+    subparser_test = subparsers.add_parser("test", parents=[common_parser])
+    subparser_test.add_argument(
+        "-n", "--name", type=str, default="Yuki Yoshida", help="Your name."
+    )
+    subparser_test.add_argument(
+        "-m",
+        "--mode",
+        type=int,
+        default=0,
+        choices=range(10),
+        metavar="NUM",
+        help="Specify test mode as an integer number.",
+    )
+    subparser_test.set_defaults(handler=test_func)
+
+    # for license
+    subparser_license = subparsers.add_parser("license", parents=[common_parser])
+    subparser_license.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="license_list.csv",
+        help="Output license list file name.",
+    )
+    subparser_license.add_argument(
+        "-t",
+        "--target",
+        type=str,
+        default=None,
+        metavar="PATH",
+        help="Path to target python packages.",
+    )
+    subparser_license.set_defaults(handler=create_license_list)
+
+    args = main_parser.parse_args()
+    args.handler(args)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
