0958bf3ccfb3b15ffe85c522560e5b5060a74e79
aYukiYoshida <yuki.yoshida@access-company.com>
2022-10-14 15:25:57 +0900

    [add] Implement class of Record

---
+++ pyigata/record.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+from dataclasses import dataclass
+
+@dataclass
+class Record:
+    _record: list[str]
+    status: int = 0
+
+    def __post_init__(self):
+        self._index: int = 0
+
+    def __iter__(self):
+        # __next__メソッドが定義されているオブジェクトを返すように定義する
+        return self
+
+    def __next__(self):
+        if self._index == len(self._record):
+            raise StopIteration()
+        data = self._record[self._index]
+        self._index += 1
+        return data
+
+    def __getitem__(self, i):
+        return self._record[i]
+
+    @classmethod
+    def generate(cls, message: str, status:int) -> Record:
+        return cls([message], status)
