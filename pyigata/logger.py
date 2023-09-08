55bc18b094773414822586b41655f068e84dd13f
aYukiYoshida <yuki.yoshida@access-company.com>
2023-09-08 13:28:24 +0900

    [mod] Prevent deplicated output of log

--- pyigata/logger.py
+++ pyigata/logger.py
@@ -16,18 +16,19 @@ class LogLevel(IntEnum):
 def get_logger(name: str, level: int = 2) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.getLevelName(LogLevel(level).name.upper()))
-    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", datefmt=r"%Y-%m-%dT%H:%M:%S%z")
-    # INFO以下のログを標準出力する
-    stdout_handler = logging.StreamHandler(stream=sys.stdout)
-    stdout_handler.setLevel(logging.DEBUG)
-    stdout_handler.setFormatter(formatter)
-    stdout_handler.addFilter(lambda record: record.levelno <= logging.INFO)
-    logger.addHandler(stdout_handler)
-    # WARNING以上のログを標準エラー出力する
-    stderr_handler = logging.StreamHandler(stream=sys.stderr)
-    stderr_handler.setLevel(logging.WARNING)
-    stderr_handler.setFormatter(formatter)
-    logger.addHandler(stderr_handler)
+    if not logger.hasHandlers():
+        formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", datefmt=r"%Y-%m-%dT%H:%M:%S%z")
+        # INFO以下のログを標準出力する
+        stdout_handler = logging.StreamHandler(stream=sys.stdout)
+        stdout_handler.setLevel(logging.DEBUG)
+        stdout_handler.setFormatter(formatter)
+        stdout_handler.addFilter(lambda record: record.levelno <= logging.INFO)
+        logger.addHandler(stdout_handler)
+        # WARNING以上のログを標準エラー出力する
+        stderr_handler = logging.StreamHandler(stream=sys.stderr)
+        stderr_handler.setLevel(logging.WARNING)
+        stderr_handler.setFormatter(formatter)
+        logger.addHandler(stderr_handler)
     return logger
 
 
